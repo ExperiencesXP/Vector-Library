6a4b1ec91ccba39f053b46edcdd9aec9e558b351
Experiences <113293259+ExperiencesXP@users.noreply.github.com>
2025-12-12 14:43:07 +0100

    Handle issues caused by using Real by TypeAliasing Scalar to int and float; additionally fixes setters.

--- VectorLib.py
+++ VectorLib.py
@@ -1,24 +1,29 @@
-from numbers import Real
-from math import acos,pi,sqrt,isclose
+from typing import TypeAlias
+
+from math import acos, pi, sqrt, isclose
+
+Scalar: TypeAlias = int | float
+
+
 class Vector:
-    def  __init__(self,x:Real,y:Real):
+    def __init__(self, x: Scalar, y: Scalar):
         self.__x = x
         self.__y = y
         
     @property
-    def x(self):
+    def x(self) -> Scalar:
         return self.__x
     
     @x.setter
-    def(self, comp:Real):
+    def x(self, comp: Scalar) -> None:
         self.__x = comp
     
     @property
-    def y(self):
+    def y(self) -> Scalar:
         return self.__y
     
     @y.setter
-    def(self, comp:Real):
+    def y(self, comp: Scalar) -> None:
         self.__y = comp
     
     def __repr__(self):
@@ -27,16 +32,16 @@ class Vector:
     def __str__(self):
         return f"{self.__x}, {self.__y}"
     
-    def __add__(self,vec:'Vector'):
+    def __add__(self, vec: 'Vector') -> 'Vector':
         return Vector((self.__x + vec.__x),(self.__y + vec.__y))
     
-    def __sub__(self,vec:'Vector'):
+    def __sub__(self, vec: 'Vector') -> 'Vector':
         return Vector((self.__x - vec.__x),(self.__y - vec.__y))
     
-    def __mul__(self, k:Real):
+    def __mul__(self, k: Scalar) -> 'Vector':
         return Vector(self.__x * k, self.__y * k)
     
-    def __truediv__(self, k:Real):
+    def __truediv__(self, k: Scalar) -> 'Vector':
         try:
             return self * (1/k)
         except:
@@ -47,32 +52,32 @@ class Vector:
             return NotImplemented
         return self.__x == other.__x and self.__y == other.__y
     
-    def almost_equal(self, other: 'Vector', abs_tol:float = 1e-9, rel_tol:float = 1e-9):
+    def almost_equal(self, other: 'Vector', abs_tol: float = 1e-9, rel_tol: float = 1e-9):
         if not isinstance(other, Vector):
             return NotImplemented
         return (isclose(self.__x, other.__x, rel_tol=rel_tol, abs_tol=abs_tol) and isclose(self.__y, other.__y, rel_tol=rel_tol, abs_tol=abs_tol))
-    
-    def length(self):
+
+    def length(self) -> float:
         return sqrt(self.__x**2 + self.__y**2)
     
-    def distance(self,vec:'Vector'):
+    def distance(self, vec: 'Vector') -> float:
         return (self-vec).length()
     
-    def normalize(self):
+    def normalize(self) -> 'Vector':
         try:
             return self / self.length()
         except:
             raise ValueError
         
-    def dotProduct(self,vec:'Vector'):
-        return self.__x*vec.__x + self.__y*vec.__y
+    def dotProduct(self, vec: 'Vector') -> float:
+        return float(self.__x)*float(vec.__x) + float(self.__y)*float(vec.__y)
     
-    def limit(self,tallimit:Real):
+    def limit(self, tallimit: Scalar) -> 'Vector':
         if not self.length() > tallimit:
             return self
         return self.normalize() * tallimit
     
-    def angleBetween(self,vec:'Vector',useDegrees:bool=False):
+    def angleBetween(self, vec: 'Vector', useDegrees: bool = False) -> float:
         if useDegrees:
             return acos(self.dotProduct(vec)/(self.length() * vec.length()))*(180/pi)
         else:
